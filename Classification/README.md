a198e6e86d0ad680bc86899f6a644d9ae5ea47be
Jiaqi Yin <jiaqiyin@yahoo.com>
2024-09-09 01:00:38 -0700

    done the dataset 1 preprocessing

---
+++ Classification/README.md
@@ -0,0 +1,52 @@
+# Content
+- 
+
+# To Start
+1. Clone the repository
+2. Download the datasets from the links provided below in [Dataset](#Dataset) section
+3. Create the environment ```conda env create -f environment.yml```
+
+# Dataset
+
+## Bank Marketing Campaign Subscription
+Source: [Download from Kaggle](https://www.kaggle.com/datasets/pankajbhowmik/bank-marketing-campaign-subscriptions)
+
+### Description
+The dataset contains information about marketing campaigns that were conducted via phone calls from a Portuguese banking institution to their clients. Purpose of these campaigns is to prompt their clients to subscribe for a specific financial product of the bank (term deposit). After each call was conducted, the client had to inform the institution about their intention of either subscribing to the product (indicating a successful campaign) or not (unsucessful campaign).
+The final output of this survey will be a binary result indicating if the client subscribed ('yes') to the product or not ('no').
+
+The dataset has 41188 rows (instances of calls to clients) and 21 columns (variables) which are describing certain aspects of the call. Please note that there are cases where the same client was contacted multiple times - something that practically doesn't affect the analysis as each call will be considered independent from another even if the client is the same.
+
+### Data Schema
+The predictor variables (features) contained in the dataset can be divided into the following five sections:
+
+1. Variables that describing attributes related directly to the client:
+    - age
+    - job: type of job (e.g. 'admin', 'technician', 'unemployed', etc)
+    - marital: marital status ('married', 'single', 'divorced', 'unknown')
+    - education: level of education ('basic.4y', 'high.school', 'basic.6y', 'basic.9y','professional.course', 'unknown','university.degree','illiterate')
+    - default: if the client has credit in default ('no', 'unknown', 'yes')
+    - housing: if the client has housing a loan ('no', 'unknown', 'yes')
+    - loan: if the client has a personal loan ? ('no', 'unknown', 'yes')
+2. Variables related to the last contact of the current campaign:
+    - contact: type of communication ('telephone', 'cellular')
+    - month: month of last contact
+    - day_of_week: day of last contact
+    - duration: call duration (in seconds)
+
+3. Other variables related to the campaign(s):
+    - campaign: number of contacts performed during this campaign and for this client 
+    - pdays: number of days passed by after the client was last contacted from a previous campaign 
+    - previous: number of contacts performed before this campaign and for this client 
+    - poutcome: outcome of previous marketing campaign ('nonexistent', 'failure', 'success')
+
+4. Socioeconomic variables:
+    - emp.var.rate: employement variation rate - quarterly indicator
+    - cons.price.idx: consumer price index - monthly indicator
+    - cons.conf.idx: consumer confidence index - monthly indicator
+    - euribor3m: euribor 3 month rate - daily indicator 
+    - nr.employed: number of employees - quarterly indicator
+
+5. Output variable (desired target): subscribed, 'Yes' or 'No'
+## YouTube Comments Spam
+- [Youtube Comments Spam Dataset](https://www.kaggle.com/datasets/ahsenwaheed/youtube-comments-spam-dataset)
---
+++ Classification/src/dataset1_tabular/preprocessing.py
@@ -0,0 +1,145 @@
+"""
+This file contains:
+1. handling outlies using the IQR method
+2. Preprocessing pipeline includes:
+    - encoding categorical features
+    - scaling numerical features
+3. train-testing splitting
+"""
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, OneHotEncoder, FunctionTransformer
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from scipy import sparse
+import numpy as np
+import joblib
+import os
+
+
+NUM_FEATURES = ['age', 'duration', 'campaign', 'previous', 'emp.var.rate', 'cons.price.idx', 'cons.conf.idx',
+                'euribor3m', 'nr.employed']
+CAT_FEATURES = ['job', 'marital', 'education', 'day_of_week', 'default', 'housing', 'loan', 'contact', 'month',
+                'poutcome', 'pdays']
+TARGET_COLUMN = 'subscribed'
+
+def load_csv(file_path):
+    return pd.read_csv(file_path, sep=';')
+
+
+def convert_target(df, column):
+    df[column] = df[column].map({
+        'yes': 1,
+        'no': -1
+    })
+    return df
+
+
+def handle_outliers(df, columns, threshold=2):
+    for col in columns:
+        q1 = df[col].quantile(0.25)
+        q3 = df[col].quantile(0.75)
+        iqr = q3 - q1
+        lb = q1 - threshold * iqr
+        ub = q3 + threshold * iqr
+        df[col] = df[col].clip(lb, ub)
+        return df
+
+
+def covert_pdays_to_categorical(df):
+    def categorize_pdays(pdays):
+        if pdays == 999:
+            return 'NoContact'
+        elif 0 <= pdays <= 2:
+            return 'Pdays0_2'
+        elif 3 <= pdays <= 7:
+            return 'Pdays3_7'
+        elif 8 <= pdays <= 14:
+            return 'Pdays8_14'
+        else:
+            return 'Pdays>14'
+
+    df_ = df.copy()
+    df_['pdays'] = df_['pdays'].apply(categorize_pdays)
+    return df_
+
+
+def create_preprocessing_pipeline(num_features, cat_features):
+    num_transformer = Pipeline(steps=[
+        ('scaler', StandardScaler())
+    ])
+
+    pdays_transformer = Pipeline(steps=[
+        ('categorize', FunctionTransformer(covert_pdays_to_categorical, validate=False)),
+        ('onehot', OneHotEncoder(handle_unknown='ignore'))
+    ])
+
+    cat_transformer = Pipeline(steps=[
+        ('onehot', OneHotEncoder(handle_unknown='ignore'))
+    ])
+
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ('num', num_transformer, num_features),
+            ('pdays', pdays_transformer, ['pdays']),
+            ('cat', cat_transformer, [col for col in cat_features if col != 'pdays'])
+        ]
+    )
+
+    return preprocessor
+
+
+def preprocess_data(df, target_column, num_features, cat_features):
+
+    df = convert_target(df, target_column)
+
+    # handle outliers
+    df = handle_outliers(df, num_features)
+
+    preprocessor = create_preprocessing_pipeline(num_features, cat_features)
+
+    # data split
+    X = df.drop(target_column, axis=1)
+    y = df[target_column]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=17)
+
+    X_train_processed = preprocessor.fit_transform(X_train)
+    X_test_processed = preprocessor.transform(X_test)
+
+    return X_train_processed, X_test_processed, y_train, y_test, preprocessor
+
+
+def save_processed_data(X_train, X_test, y_train, y_test, preprocessor, output_dir):
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    sparse.save_npz(os.path.join(output_dir, 'X_train.npz'), X_train)
+    sparse.save_npz(os.path.join(output_dir, 'X_test.npz'), X_test)
+    np.save(os.path.join(output_dir, 'y_train.npy'), y_train)
+    np.save(os.path.join(output_dir, 'y_test.npy'), y_test)
+    joblib.dump(preprocessor, os.path.join(output_dir, 'preprocessor.joblib'))
+
+    print('Data saved successfully!')
+
+
+
+def main(file_path, output_dir):
+    df = load_csv(file_path)
+    X_train, X_test, y_train, y_test, preprocessor = preprocess_data(df, TARGET_COLUMN, NUM_FEATURES, CAT_FEATURES)
+
+    print(f"X_train shape: {X_train.shape}")
+    print(f"X_test shape: {X_test.shape}")
+
+    # save
+    save_processed_data(X_train, X_test, y_train, y_test, preprocessor, output_dir)
+    return X_train, X_test, y_train, y_test, preprocessor
+
+if __name__ == '__main__':
+    file_path = '../../data/dataset1/raw/Bank_Campaign.csv'
+    output_dir = '../../data/dataset1/processed'
+
+    main(file_path, output_dir)
+
+
