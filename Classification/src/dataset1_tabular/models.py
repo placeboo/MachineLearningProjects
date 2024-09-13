9e670187cbcd212cd86b1b139adbeca21f4da19b
Jiaqi Yin <jiaqiyin@yahoo.com>
2024-09-13 01:39:48 -0700

    add the fit time and start svm

--- Classification/src/dataset1_tabular/models.py
+++ Classification/src/dataset1_tabular/models.py
@@ -37,3 +37,36 @@ class KNNModel:
         y_pred = self.best_model.predict(self.X_test)
         evaluation_metrics = evaluate_model(self.y_test, y_pred)
         return evaluation_metrics
+
+class SVMModel:
+    def __init__(self, X_train, y_train, X_test, y_test, cv, param_grid, random_state=17):
+        self.X_train = X_train
+        self.y_train = y_train
+        self.X_test = X_test
+        self.y_test = y_test
+        self.cv = cv
+        self.param_grid = param_grid
+        self.model = SVC()
+        self.random_state = random_state
+        self.best_model = None
+        self.cv_results = None
+        self.best_params = None
+
+    def train(self):
+        grid_search = GridSearchCV(self.model, param_grid=self.param_grid, cv=self.cv, n_jobs=-1, return_train_score=True, verbose=4)
+
+        grid_search.fit(self.X_train, self.y_train)
+
+        print(f'Best parameters: {grid_search.best_params_}')
+        print(f'Best score: {grid_search.best_score_: .3f}')
+
+        # log the best model and evaluate
+        best_model = grid_search.best_estimator_
+        self.best_model = best_model
+        self.cv_results = grid_search.cv_results_
+        self.best_params = grid_search.best_params_
+
+    def evaluate(self):
+        y_pred = self.best_model.predict(self.X_test)
+        evaluation_metrics = evaluate_model(self.y_test, y_pred)
+        return evaluation_metrics
