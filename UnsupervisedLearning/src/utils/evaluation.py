1b14fc61653c9bd9eb36f68da1e537fae1b34657
Jiaqi Yin <jiaqiyin@yahoo.com>
2024-11-03 00:13:17 -0700

    done experiment 4 experiment

--- UnsupervisedLearning/src/utils/evaluation.py
+++ UnsupervisedLearning/src/utils/evaluation.py
@@ -1,4 +1,8 @@
 import numpy as np
+from typing import Dict, Tuple
+
+import pandas as pd
+
 
 def find_elbow_indice(arr: np.ndarray) -> int:
     """
@@ -11,3 +15,46 @@ def find_elbow_indice(arr: np.ndarray) -> int:
     # Find the index of the maximum value of the second derivative
     elbow_idx = np.argmin(np.abs(second_derivative)) + 2
     return elbow_idx
+
+
+def organize_experiment4_results(data: Dict) -> Tuple[pd.DataFrame, pd.DataFrame, int]:
+    """
+    Organize the result of experiment 4.
+    :return:
+    the top 1 highest test score
+    """
+    results = {
+        'n_components': [],
+        'mean_fit_time': [],
+        'mean_score_time': [],
+        'mean_train_score': [],
+        'mean_test_score': [],
+        'params':[]
+    }
+
+    eval_result = []
+
+    for n_comp, values in data.items():
+        cv_results = values['cv_results']
+        cv_results = pd.DataFrame(cv_results)
+        # find the top 1 highest test score
+        cv_result = cv_results.loc[cv_results['rank_test_score'] == 1]
+        results['n_components'].append(n_comp)
+        results['params'].append(cv_result['params'].values[0])
+        results['mean_fit_time'].append(cv_result['mean_fit_time'].values[0])
+        results['mean_score_time'].append(cv_result['mean_score_time'].values[0])
+        results['mean_test_score'].append(cv_result['mean_test_score'].values[0])
+        results['mean_train_score'].append(cv_result['mean_train_score'].values[0])
+
+        tmp_eval_result = {
+            'n_components': n_comp,
+            **values['metrics']
+        }
+        eval_result.append(tmp_eval_result)
+    result_df = pd.DataFrame(results)
+    eval_result_df = pd.DataFrame(eval_result)
+
+    # find the optimal n_components with the highest test score
+    best_idx = result_df['mean_test_score'].idxmax()
+    best_n_components = result_df.loc[best_idx, 'n_components']
+    return result_df, eval_result_df, best_n_components
